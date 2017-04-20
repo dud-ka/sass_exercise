91945c3d87d9deb0546b289b1711e7d4b042a6a9
Karolina Dudzik <kaardud@gmail.com>
2017-04-20 22:48:45 +0200

    GRUNT

---
+++ kodilla-grunt.js
@@ -0,0 +1,53 @@
+module.exports = function(grunt) {
+
+  // Project configuration.
+  grunt.initConfig({
+  	sass: {
+  		options: {
+  			sourceMap: true
+  		},
+  		dist: {
+  			files: {
+  				'style.css': 'style.sass'
+  			}
+  		}
+  	},
+
+  	imagemin: {
+  		dynamic: {
+  			files: [{
+  				expand: true,
+  				cwd: 'images/',
+  				src: ['**/*.{png,jpg,gif}'],
+  				dest: 'images/build/'
+  			}]
+  		}
+  	},
+  	watch: {
+  		scripts: {
+  			files: ['*.sass'],
+  			tasks: ['sass'],
+  			options: {
+  				spawn: false,
+  			}
+  		} 
+  	},
+  	browserSync: {
+  		dev: {
+  			bsFiles: {
+  				src : [
+  				'*.css',
+  				'*.html'
+  				]
+  			}
+  		}
+  	}
+  });
+  // Load the plugins tasks 
+  grunt.loadNpmTasks('grunt-contrib-sass');
+  grunt.loadNpmTasks('grunt-contrib-imagemin');
+  grunt.loadNpmTasks('grunt-contrib-watch');
+  grunt.loadNpmTasks('grunt-browser-sync');
+  // Default task(s).
+  grunt.registerTask('default', ['sass', 'imagemin', 'watch']);
+};
